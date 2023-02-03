b36aa6bb3f9be50553ba7f7cff4dc5a5b987d8f8
Mike Friedel <mfriedel@gmail.com>
2023-02-03 20:36:13 +0000

    - Switched from OpenRC system to screen based for easier iperf control/visibility - Added environment variable support for iperf control

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
 FROM alpine
 
-RUN apk add --no-cache bash busybox curl iftop iperf iperf3 iptraf-ng jq open-lldp openrc tcpdump wget
-COPY lldpad-post /etc/init.d/
-RUN sed -i 's/^tty.*//; s/^#.*//;/^$/d' /etc/inittab; rc-update add lldpad; rc-update add lldpad-post
+RUN apk add --no-cache bash busybox curl iftop iperf iperf3 iptraf-ng jq open-lldp screen tcpdump wget
+COPY entrypoint.sh /
 
-ENTRYPOINT ["/sbin/init"]
-#ENTRYPOINT ["/bin/sh"]
+### Defaults ###
+#ENV LLDP_SERVER=1
+#ENV IPERF_SERVER=1
+#
+# Setting IPERF_SRVIP starts a screen tab with an iperf3 client that connects to it. 
+#ENV IPERF_SRVIP=""
+#
+#ENV IPERF_RATE="1M"
+#ENV IPERF_MSS="1300"
+#ENV IPERF_BIDIR="yes"
+#ENV IPERF_OPTIONS=""
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+SCREENRC="/root/.screenrc"
+cat << EOF > /root/.bashrc
+sleep 2
+echo "IPs: "
+ip -o addr show | cut -d' ' -f7| grep -v '127\|^::1' | cut -d'/' -f1
+EOF
+
+cat << EOF > $SCREENRC
+startup_message off
+defscrollback 1000
+caption always "%{= bb}%{+b w}%n %t %h %=%l %H %c"
+hardstatus alwayslastline "%-Lw%{= BW}%50>%n%f* %t%{-}%+Lw%<"
+activity "Activity in %t(%n)"
+shelltitle "shell"
+EOF
+
+if [ -z "$LLDP_SERVER" ] || [ ! "$LLDP_SERVER" == "0" ]; then
+    echo "Starting lldpad"
+    /usr/sbin/lldpad -d -f /etc/lldpad.conf
+    #echo "screen -t lldp /usr/sbin/lldpad -f /etc/lldpad.conf" >> $SCREENRC
+    LTOOL='/usr/sbin/lldptool'
+    for net in `ls /sys/class/net/ | grep 'et\|en'`; do
+        echo "Enabling LLDP on $net"
+        $LTOOL set-lldp -i $net adminStatus=rxtx >/dev/null
+        for tlv in `lldptool --h |grep -A100 "TLV identifiers:" | tail +2 | awk -F':' '{print $1}' | xargs`; do
+            $LTOOL -T -i $net -V $tlv enableTx=yes >/dev/null
+        done
+    done
+fi
+
+if [ -z "$IPERF_SERVER" ] || [ ! "$IPERF_SERVER" == "no" ]; then
+    echo "Starting iperf server"
+    echo "screen -t iperf /usr/bin/iperf3 -s" >> $SCREENRC
+fi
+
+[ -z "$IPERF_RATE" ] && export IPERF_RATE="1M"
+
+[ -z "$IPERF_MSS" ] && export IPERF_MSS="1300"
+if [ "$IPERF_MSS" == "yes" ]; then
+    export IPERF_OPTIONS="${IPERF_OPTIONS} -M ${IPERF_MSS}"
+fi
+
+[ -z "$IPERF_BIDIR" ] && export IPERF_BIDIR="yes"
+if [ "$IPERF_BIDIR" == "yes" ]; then
+    export IPERF_OPTIONS="${IPERF_OPTIONS} --bidir"
+fi
+
+echo "screen -t shell bash" >> $SCREENRC
+
+if [ "$IPERF_SRVIP" ]; then
+    echo "Starting iperf client"
+    echo "screen -t iperf /usr/bin/iperf3 -c $IPERF_SRVIP -t 0 -b $IPERF_RATE$IPERF_OPTIONS" >> $SCREENRC
+fi
+
+screen -c $SCREENRC
